2c1ae68f387ffc869a9c904c3d65b7dfe5b0f3b2
Krzysztof Dmitruk <kdmitruk@gmail.com>
2023-03-06 12:39:10 +0100

    step 1

--- lect2/src/Main.java
+++ lect2/src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
+
+    public static class A {
+        public Integer field = 5;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!!");
+        A a = new A();
+        A b = a;
+        a.field = 10;
+        System.out.print(b.field);
     }
 }
