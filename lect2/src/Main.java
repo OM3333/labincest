f7fff1334c5547f0e8a5afc0d33ad39f6e5b7e9a
Krzysztof Dmitruk <kdmitruk@gmail.com>
2023-03-06 12:53:18 +0100

    step 2

--- lect2/src/Main.java
+++ lect2/src/Main.java
@@ -1,12 +1,23 @@
 public class Main {
-
     public static class A {
-        public Integer field = 5;
+        public Integer getField() {
+            return field;
+        }
+        protected Integer field = 5;
+    }
+    public static class B extends A {
+        public Integer getChildField() {
+            return childField;
+        }
+        public B() {
+            this.childField = 100;
+            this.field = 200;
+        }
+        private Integer childField = 100;
     }
     public static void main(String[] args) {
-        A a = new A();
-        A b = a;
-        a.field = 10;
-        System.out.print(b.field);
+        B b = new B();
+        System.out.println(b.field);
+        System.out.println(b.getChildField());
     }
 }
