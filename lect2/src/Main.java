463a745d4b827b448749ab19981f0a2dcb511a53
Krzysztof Dmitruk <kdmitruk@gmail.com>
2023-03-06 13:29:05 +0100

    step 3

--- lect2/src/Main.java
+++ lect2/src/Main.java
@@ -1,23 +1,82 @@
+import java.sql.SQLOutput;
+
 public class Main {
-    public static class A {
-        public Integer getField() {
-            return field;
+    abstract public static class Base {
+        protected int field;
+
+        public Base(int field) {
+            this.field = field;
         }
-        protected Integer field = 5;
+
+        public void say() {
+            System.out.println("BASE "+this.field);
+        }
+
+        abstract void connect();
+
+
+
+        protected void printX() {
+            System.out.println("X");
+        }
     }
-    public static class B extends A {
-        public Integer getChildField() {
-            return childField;
+
+    public static class ChildA extends Base {
+        private int childAField;
+        public ChildA(int field, int childAField) {
+            super(field);
+            this.childAField = childAField;
         }
-        public B() {
-            this.childField = 100;
-            this.field = 200;
+
+        @Override
+        public String toString() {
+            return "ChildA{" +
+                    "childAField=" + childAField +
+                    ", field=" + field +
+                    '}';
         }
-        private Integer childField = 100;
+
+        public void say() {
+            System.out.println("CHILD_A" + this.childAField);
+            super.say();
+        }
+
+        @Override
+        void connect() {
+            System.out.println("connect as child A");
+        }
     }
+
+//    public static class ChildB extends Base {
+//        public void say() {
+//            System.out.println("CHILD_B");
+//        }
+//    }
+//
+//    public static class Orphan extends Base{
+//        public void say() {
+//            System.out.println("ORPHAN");
+//        }
+//    }
+
     public static void main(String[] args) {
-        B b = new B();
-        System.out.println(b.field);
-        System.out.println(b.getChildField());
+//        Base b = new Base(100);
+//        b.say();
+
+        ChildA ca = new ChildA(100, 200);
+        System.out.println(ca);
+
+        //ca.say();
+//        ChildB cb = new ChildB();
+//
+//        Base arr[] = new Base[4];
+//        arr[0] = b;
+//        arr[1] = ca;
+//        arr[2] = cb;
+//        arr[3] = new Orphan();
+//
+//        for(int i=0; i<arr.length; ++i)
+//            arr[i].say();
+
     }
 }
